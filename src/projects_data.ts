67a3b53218a9e2c615962b9468bc6c73ce228d6e
IsomerX <49413460+IsomerX@users.noreply.github.com>
2023-01-01 05:16:37 +0530

    new sections

---
+++ src/projects_data.ts
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    heading: 'WHITE RED SPACE',
+    image: '/assets/portfolio/whiteredspace.png',
+    mimage: '/assets/portfolio/whiteredspace-m.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus fermentum nisl in venenatis. Duis bibendum erat et diam lacinia, ut finibus neque fringilla. Nam sit amet nibh dolor. Aliquam dignissim porta quam. Maecenas eros turpis, auctor a dolor id, luctus sagittis dui. Etiam tempor enim vitae nunc convallis tristique. Aliquam erat volutpat.',
+  },
+  {
+    heading: 'STAR ATLAS',
+    image: '/assets/portfolio/staratlas.png',
+    mimage: '/assets/portfolio/staratlas-m.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus fermentum nisl in venenatis. Duis bibendum erat et diam lacinia, ut finibus neque fringilla. Nam sit amet nibh dolor. Aliquam dignissim porta quam. Maecenas eros turpis, auctor a dolor id, luctus sagittis dui. Etiam tempor enim vitae nunc convallis tristique. Aliquam erat volutpat.',
+  },
+  {
+    heading: 'STRIDE VENTURES',
+    image: '/assets/portfolio/stride_ventures.png',
+    mimage: '/assets/portfolio/stride_ventures-m.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus fermentum nisl in venenatis. Duis bibendum erat et diam lacinia, ut finibus neque fringilla. Nam sit amet nibh dolor. Aliquam dignissim porta quam. Maecenas eros turpis, auctor a dolor id, luctus sagittis dui. Etiam tempor enim vitae nunc convallis tristique. Aliquam erat volutpat.',
+  },
+  {
+    heading: 'URVI PORTFOLIO',
+    image: '/assets/portfolio/urviportfolio.png',
+    mimage: '/assets/portfolio/urviportfolio-m.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus fermentum nisl in venenatis. Duis bibendum erat et diam lacinia, ut finibus neque fringilla. Nam sit amet nibh dolor. Aliquam dignissim porta quam. Maecenas eros turpis, auctor a dolor id, luctus sagittis dui. Etiam tempor enim vitae nunc convallis tristique. Aliquam erat volutpat.',
+  },
+  {
+    heading: 'FRESOURCES',
+    image: '/assets/portfolio/fresources.png',
+    mimage: '/assets/portfolio/fresources-m.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus fermentum nisl in venenatis. Duis bibendum erat et diam lacinia, ut finibus neque fringilla. Nam sit amet nibh dolor. Aliquam dignissim porta quam. Maecenas eros turpis, auctor a dolor id, luctus sagittis dui. Etiam tempor enim vitae nunc convallis tristique. Aliquam erat volutpat.',
+  },
+
+  {
+    heading: 'TWATTER',
+    image: '/assets/portfolio/twatter.png',
+    mimage: '/assets/portfolio/twatter-m.png',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus fermentum nisl in venenatis. Duis bibendum erat et diam lacinia, ut finibus neque fringilla. Nam sit amet nibh dolor. Aliquam dignissim porta quam. Maecenas eros turpis, auctor a dolor id, luctus sagittis dui. Etiam tempor enim vitae nunc convallis tristique. Aliquam erat volutpat.',
+  },
+]
