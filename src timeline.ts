f795e2982261e953e2ebbf8ec69fa57eccf01695
kali user <hackit@kali.root>
2023-03-26 03:06:34 +0530

    adding timeline

---
+++ src timeline.ts
@@ -0,0 +1,58 @@
+const TIMELINE = {
+	2023: [
+		{
+			logo: "",
+			name: "Yellow Sapphire Technologies",
+			time_s: "May 2022",
+			time_e: "Present",
+			role: "Lead Frontend Developer, Part-time",
+			desc: ""
+		},
+		{
+			logo: "",
+			name: "Fresources",
+			time_s: "Jan 2022",
+			time_e: "Present",
+			role: "Co-Founder, CTO",
+			desc: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+		}
+	],
+	2022: [
+		{
+			logo: "",
+			name: "Bela Fasonado",
+			time_s: "Jun 2022",
+			time_e: "Aug 2022",
+			role: "Freelance Web Developer",
+			desc: "",
+		},
+		{
+			logo: "",
+			name: "Vance (YC W22)",
+			time_s: "Mar 2022",
+			time_e: "Apr 2022",
+			role: "Fronend Developer, Internship",
+			desc: "",
+		}
+	],
+	2021: [
+		{
+			logo: "",
+			name: "Stride Ventures",
+			time_s: "Jun 2022",
+			time_e: "July 2022",
+			role: "Freelance Web Developer",
+			desc: ""
+		},
+		{
+			logo: "",
+			name: "White Red Space",
+			time_s: "Nov 2020",
+			time_e: "Feb 2021",
+			role: "Freelance Web Developer",
+			desc: ""
+		}
+	]
+}
+
+export default TIMELINE
